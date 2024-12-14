bdf192f0bfa879a9f8c05416706444625edf7e0d
syn <94387725+ilylucas@users.noreply.github.com>
2024-12-14 15:30:54 -0300

    Update Download.jsx

--- components/pages/Download.jsx
+++ components/pages/Download.jsx
@@ -6,10 +6,10 @@ const Download = () => {
     <section id='download-section' className='w-screen flex-col h-screen flex justify-center py-16 items-center overflow-x-hidden'>
 
     <div className='flex flex-col gap-8 justify-center items-center'>
-        <h1 className='text-4xl'>meet the revolutionary scripting utility</h1>
+        <h1 className='text-4xl'>ready to dive in the leading macos exploit?</h1>
         <button className='flex gap-4 home-div z-10 px-6 py-3 rounded-full bg-black/10 border-[1px] border-white/10 text-white/80 hover:bg-black/20 duration-200'>
             <LucideDownload/>
-            get started
+            download
         </button>
         
         <div className='bg-[#4000FF] absolute w-96 h-96 blur-[500px] translate-y-1/2 rounded-full'/>
